399ed71165948a31bf83e3941ab44ad184b5ab26
Eric Grimard <e.c.grimard@gmail.com>
2017-01-10 18:47:50 -0500

    no message

---
+++ scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	if(document.getElementById("ENoption").checked)
+	{
+		$(".EN").show();
+		$(".FR").hide();
+	}
+	else
+	{
+		$(".EN").hide();
+		$(".FR").show();
+	}
+});
+function toggleFR()
+{
+alert();	
+}
